583bade34d49285fcc5f75c3a046442c71d2cfe1
Abir Chebbi <abirc240@gmail.com>
2024-11-11 15:23:38 +0100

    Added training parameters and paths to config.py

---
+++ python/config.py
@@ -0,0 +1,7 @@
+# path to all_words
+all_words_path = 'data/all_words.txt'
+
+# training parameters 
+start_epoch = 56
+start_batch = 0
+batch_size = 25  # Related to how your batches are handled in the model (e.g., self.batch_size)
