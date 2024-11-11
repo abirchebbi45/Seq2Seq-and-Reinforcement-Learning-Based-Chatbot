25e17f1a5a3d22c4b8a22625f00cd29ad26bf67f
Abir Chebbi <abirc240@gmail.com>
2024-11-11 16:20:10 +0100

    Added Data_Reader class with conversation processing functionality

---
+++ python/data_reader.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import cPickle as pickle
+import config
+import random
+
+class Data_Reader:
+    def __init__(self, cur_train_index=0, load_list=False):
+        self.training_data = pickle.load(open(config.training_data_path, 'rb'))
+        self.data_size = len(self.training_data)
+        if load_list:
+            self.shuffle_list = pickle.load(open(config.index_list_file, 'rb'))
+        else:    
+            self.shuffle_list = self.shuffle_index()
+        self.train_index = cur_train_index
