c016080a5f4e6341e19400be4be3cfdb346a10a1
Abir Chebbi <abirc240@gmail.com>
2024-11-11 16:31:25 +0100

    Add shuffle index list configurations to config file

--- python/config.py
+++ python/config.py
@@ -5,3 +5,8 @@ all_words_path = 'data/all_words.txt'
 start_epoch = 56
 start_batch = 0
 batch_size = 25  # Related to how your batches are handled in the model (e.g., self.batch_size)
+
+# data reader shuffle index list
+load_list = False
+index_list_file = 'data/shuffle_index_list'
+cur_train_index = start_batch * batch_size
